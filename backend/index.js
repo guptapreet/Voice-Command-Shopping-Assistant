50277030c207aaf7674be941dad16edcbef67456
guptapreet <preetgupta464@gmail.com>
2025-02-11 02:06:18 +0530

    updated port

--- backend/index.js
+++ backend/index.js
@@ -15,5 +15,5 @@ mongoose
 
 app.use("/api", routes);
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
