37ec95ef307f63d5ff8392e1f455e36a1cedeb66
guptapreet <preetgupta464@gmail.com>
2025-02-11 02:50:59 +0530

    update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,15 @@
-require("dotenv").config();
 const express = require("express");
-const mongoose = require("mongoose");
-const routes = require("./routes");
+const cors = require("cors");
 
 const app = express();
-app.use(express.json());
 
-mongoose
-    .connect(process.env.MONGO_URI, {
+app.use(cors());
 
+
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+        methods: "GET,POST,PUT,DELETE",
+        allowedHeaders: "Content-Type,Authorization",
     })
-    .then(() => console.log("MongoDB connected successfully"))
-    .catch((err) => console.error("MongoDB connection error:", err));
-
-app.use("/api", routes);
-
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+);
